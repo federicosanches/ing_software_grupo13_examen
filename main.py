0b0adacc52f9f7eed08917db95552e9beb3bc95a
ceci-podesta <ceciliaxpodesta@gmail.com>
2025-10-25 11:28:44 -0300

    archivo main con codigo para manejo de guardado data en json

---
+++ main.py
@@ -0,0 +1,47 @@
+import json
+
+from fastapi import FastAPI
+
+STATUS = "status"
+AMOUNT = "amount"
+PAYMENT_METHOD = "payment_method"
+
+STATUS_REGISTRADO = "REGISTRADO"
+STATUS_PAGADO = "PAGADO"
+STATUS_FALLIDO = "FALLIDO"
+
+DATA_PATH = "data.json"
+
+app = FastAPI()
+
+
+def load_all_payments():
+    with open(DATA_PATH, "r") as f:
+        data = json.load(f)
+    return data
+
+
+def save_all_payments(data):
+    with open(DATA_PATH, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+def load_payment(payment_id):
+    data = load_all_payments()[payment_id]
+    return data
+
+
+def save_payment_data(payment_id, data):
+    all_data = load_all_payments()
+    all_data[str(payment_id)] = data
+    save_all_payments(all_data)
+
+
+def save_payment(payment_id, amount, payment_method, status):
+    data = {
+        AMOUNT: amount,
+        PAYMENT_METHOD: payment_method,
+        STATUS: status,
+    }
+    save_payment_data(payment_id, data)
+
